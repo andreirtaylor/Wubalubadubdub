768576d6ea3ff33f5604cbd9097acbed4677af92
Jake <jake.elijah.cooper@gmail.com>
2016-02-20 15:28:41 -0800

    Food normalized and calculated

--- app/tests/food.py
+++ app/tests/food.py
@@ -1,19 +1,30 @@
 WEIGHT  = 10
 
+import math
+
 # Returns a positive floating point between 0 and 1 that will be multipled by weight and and the current score
 # Return None if you do not want the snake to go in this direction
 def run(data, direction):
     head = [x for x in data["snakes"] if x["id"] == data["our-snake-id"]][0]["coords"][0]
-    myweight = 0
+    myWeight = 0
+    numFood = 0
     for food in data["food"]:
     	if foodInDirectionOfHead(head,food,direction):
-    		myweight += 1 / float(calculateMoves(head,food)) 
+    		numFood += 1
+    		myWeight += 1/float(calculateMoves(head,food, data["height"],data["width"]))
     #moves = {"north": 0.1, "south": 0.20, "east": 0.3, "west": 0.4}
-    return myweight
+    if numFood is 0:
+    	return 0
+    else:
+    	return myWeight / (math.log(numFood + 1) * 2)	
+    return myWeight
     
 
-def calculateMoves(head,point):
-    return abs(head[0] - point[0]) + abs(head[1] - point[0])
+def calculateMoves(head,point,height,width):
+	# xDist = abs(head[0] - point[0])
+	# yDist = abs(head[1] - point[0])
+	# return xDist + yDist
+    return abs(head[0] - point[0]) + abs(head[1] - point[1])
 
 def foodInDirectionOfHead(head,point, direction):
 	if direction == "north":
@@ -39,4 +50,4 @@ def foodInDirectionOfHead(head,point, direction):
  		
 
 if __name__ == '__main__':
-    print run({"food":[[0,3],[3,2],[2,2]],"snakes": [{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8","coords":[[3,1]]}], "our-snake-id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8"},"west")	
+    print run({"width": 17, "height":17, "food":[[0,0]],"snakes": [{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8","coords":[[3,1]]}], "our-snake-id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8"},"west")	
