1d7bb40028542af0a33059e3048c2b9a2192440a
andrei <andreirtaylor@gmail.com>
2016-02-20 12:48:07 -0800

    ad obstacles

---
+++ app/tests/obstacles.py
@@ -0,0 +1,7 @@
+WEIGHT  = 1
+
+# Returns a positive integer that will be multipled by weight and and the current score
+# Return None if you do not want the snake to go in this direction
+
+def run(data, direction):
+    print data
