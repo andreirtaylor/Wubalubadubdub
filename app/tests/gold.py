a85ea8562b3d772e5f4036713409ae6e46d5e607
Jonah Boretsky <jonah.boretsky@gmail.com>
2016-02-20 14:20:30 -0800

    test

--- app/tests/gold.py
+++ app/tests/gold.py
@@ -5,7 +5,13 @@ WEIGHT  = 10
 def run(data, direction):
     # setup gold holder and my_head
     gold = []
-    my_head = [x for x in data["snakes"] if x["id"] == data[""]][0]["coords"][0]
+    my_head = []
+
+    for x in data["snakes"]:
+        if x["id"] == "9a6b2c23-9485-4d7b-b459-d0a8689e10d8":
+            my_head = x["coords"][0]
+
+    #my_head = [x for x in data["snakes"] if x["id"] == data["our_snake_id"]][0]["coords"][0]
 
     print(my_head)
     
