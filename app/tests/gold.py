2997d7848614c9097a29b9a5df9f1c0ffde1aa01
Jonah Boretsky <jonah.boretsky@gmail.com>
2016-02-20 15:58:24 -0800

    fix gold

--- app/tests/gold.py
+++ app/tests/gold.py
@@ -3,6 +3,9 @@ WEIGHT  = 10
 # Returns a positive integer that will be multipled by weight and and the current score
 # Return None if you do not want the snake to go in this direction
 def run(data, direction):
+    if data.get("gold") == None:
+        return 0
+
     # setup gold holder
     gold = []
 
