4e639d067a7c86d38ef133cb6e531a985395505d
Jake <jake.elijah.cooper@gmail.com>
2016-02-20 21:59:32 -0800

    Added collision detection

--- app/tests/headoncollision.py
+++ app/tests/headoncollision.py
@@ -7,11 +7,42 @@ def run(data, direction):
     ourSnake = [x for x in data["snakes"] if x["id"] == data["our-snake-id"]][0]
     ourHead = ourSnake["coords"][0][:]
     ourSize = len(ourSnake["coords"])
+    otherSnakeHeads = [tuple(x["coords"][0]) for x in data["snakes"] if x["id"] != data["our-snake-id"]]
     for snake in data["snakes"]:
         if snake["id"] is not data["our-snake-id"]:
             theirHead = snake["coords"][0]
+            checkSquares = []
             if calculateMoves(ourHead, theirHead) == 2 and len(snake["coords"]) >= ourSize:
-                return None
+                if direction is "north":
+                    potentialHead = [ourHead[0], ourHead[1]-1]
+                    checkSquares.append(tuple([potentialHead[0] - 1, potentialHead[1]])) #west
+                    checkSquares.append([tuple(potentialHead[0] + 1, potentialHead[1])]) #east
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] - 1]))  #north
+                    #checkSquares.append(tuple([potentialHead[0], potentialHead[1] + 1]))  #south
+                elif direction is "east":
+                    potentialHead = [ourHead[0] + 1, ourHead[1]]
+                    #checkSquares.append(tuple([potentialHead[0] - 1, potentialHead[1]])) #west
+                    checkSquares.append([tuple(potentialHead[0] + 1, potentialHead[1])]) #east
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] - 1]))  #north
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] + 1]))  #south
+                elif direction is "south":
+                    potentialHead = [ourHead[0], ourHead[1] + 1]
+                    checkSquares.append(tuple([potentialHead[0] - 1, potentialHead[1]])) #west
+                    checkSquares.append([tuple(potentialHead[0] + 1, potentialHead[1])]) #east
+                    #checkSquares.append(tuple([potentialHead[0], potentialHead[1] - 1]))  #north
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] + 1]))  #south
+                else:
+                    potentialHead = [ourHead[0] - 1, ourHead[1]]
+                    checkSquares.append(tuple([potentialHead[0] - 1, potentialHead[1]])) #west
+                    #checkSquares.append([tuple(potentialHead[0] + 1, potentialHead[1])]) #east
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] - 1]))  #north
+                    checkSquares.append(tuple([potentialHead[0], potentialHead[1] + 1]))  #south
+
+                if (set(checkSquares).intersection(otherSnakeHeads) > 0):
+                    return None
+                else:
+                    return 1    
+
     		#When were at a crossroads, check the size of the snake.
     return 1
 
@@ -19,4 +50,4 @@ def calculateMoves(head,point):
     return abs(head[0] - point[0]) + abs(head[1] - point[1])
 
 if __name__ == '__main__':
-    print run({"width": 17, "height":17, "food":[[2,1]],"snakes": [{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8","coords":[[2,1]]},{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d7","coords":[[4,1]]}], "our-snake-id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8"},"west")
+    print run({"width": 17, "height":17, "food":[[3,1]],"snakes": [{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8","coords":[[3,1]]},{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d7","coords":[[2,2]]},{"id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d6","coords":[[0,4]]}], "our-snake-id":"9a6b2c23-9485-4d7b-b459-d0a8689e10d8"},"west")
