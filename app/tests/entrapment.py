a70371db4c9c2f60c408a3f067998ccb83132b52
Chris Kelly <cpjkelly@gmail.com>
2016-02-20 17:20:45 -0800

    Remove comments

--- app/tests/entrapment.py
+++ app/tests/entrapment.py
@@ -1,6 +1,3 @@
-# Returns a positive integer that will be multipled by weight and and the current score
-# Return None if you do not want the snake to go in this direction
-
 def run(data, direction):
     head = head(data)
     next_loc = move(direction, head)
