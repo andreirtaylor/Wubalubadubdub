f6b2c364ea18644ce7f108f3f393ada9c9dbdb4a
Jake <jake.elijah.cooper@gmail.com>
2016-02-20 22:58:52 -0800

    More debuggin of collision stuff

--- app/tests/headoncollision.py
+++ app/tests/headoncollision.py
@@ -12,7 +12,7 @@ def run(data, direction):
         if snake["id"] is not data["our-snake-id"]:
             theirHead = snake["coords"][0]
             checkSquares = []
-            if calculateMoves(ourHead, theirHead) == 2 and len(snake["coords"]) >= ourSize:
+            if calculateMoves(ourHead, theirHead) <= 2 and len(snake["coords"]) >= ourSize:
                 print("Direct is " + direction)
                 if direction is "north":
                     potentialHead = [ourHead[0], ourHead[1]-1]
